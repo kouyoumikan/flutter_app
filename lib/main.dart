6fd2fd38348c33110a1caded54ac597bc50a31f4
小林英里香 <kouyoumikan136@gmail.com>
2020-04-07 16:14:05 +0900

    2020/04/07　#12 CrossAxisAlignment デフォルト値(列の中央に配置)

--- lib/main.dart
+++ lib/main.dart
@@ -18,8 +18,8 @@ class Home extends StatelessWidget {
       body: Column(
         // 列の一番下に配置する
         mainAxisAlignment: MainAxisAlignment.end,
-        // 利用可能なスペースだけ横に伸びる
-        crossAxisAlignment: CrossAxisAlignment.stretch,
+        // デフォルト値(列の中央に配置)
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: <Widget>[
           Container(
             padding: EdgeInsets.all(20.0),
